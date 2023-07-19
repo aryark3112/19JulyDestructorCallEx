8ddd7f02104452e20f0ff0b53563b92123df7d6f
aryark <aryark3112@gmail.com>
2023-07-19 12:38:25 +0530

    first commit

---
+++ Emp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace _19JulyDestructorEx
+{
+    public class Emp
+    {
+        string name;
+        public Emp(string name)
+        {
+            this.name = name;
+            Console.WriteLine("Employee Constructor Call");
+            Console.WriteLine("Stored Name: \t" + name);
+        }
+        Emp()
+        {
+            Console.WriteLine("Destructor Call & Name is: \t" +name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19JulyDestructorEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Emp Obj = new Emp("Sam");
+            Console.ReadKey();
+        }
+    }
+}
